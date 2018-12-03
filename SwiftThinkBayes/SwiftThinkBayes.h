fae922861705dffdd451844398cc79bcb7761b48
Matthew Walker <mattgwwalker@gmail.com>
2018-12-03 20:36:33 +1300

    Initial Commit

---
+++ SwiftThinkBayes/SwiftThinkBayes.h
@@ -0,0 +1,19 @@
+//
+//  SwiftThinkBayes.h
+//  SwiftThinkBayes
+//
+//  Created by Matthew Walker on 3/12/18.
+//  Copyright © 2018 Matthew Walker. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftThinkBayes.
+FOUNDATION_EXPORT double SwiftThinkBayesVersionNumber;
+
+//! Project version string for SwiftThinkBayes.
+FOUNDATION_EXPORT const unsigned char SwiftThinkBayesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftThinkBayes/PublicHeader.h>
+
+
