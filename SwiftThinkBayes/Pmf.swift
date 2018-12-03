4ef99d302065b6d94c3ca93edd641bd38aab520e
Matthew Walker <mattgwwalker@gmail.com>
2018-12-03 22:29:35 +1300

    Added Pmf.normalize()

--- SwiftThinkBayes/Pmf.swift
+++ SwiftThinkBayes/Pmf.swift
@@ -28,8 +28,51 @@ class DictWrapper<T: Hashable> {
         
         dict[key] = value * factor
     }
+    
+    /**
+     Returns the total of the frequencies/probabilities in the dict.
+     */
+    func total() -> Double {
+        var total: Double = 0.0
+        
+        for (key,value) in dict {
+            total += value
+            dict[key] = value
+        }
+        
+        return total
+    }
 
 }
 
 class Pmf<T: Hashable>: DictWrapper<T> {
+    enum Errors : Error {
+        case TotalProbabilityZero
+    }
+    
+    /**
+     Normalizes this PMF so the sum of all probs is fraction.
+     
+     - Parameters:
+     -fraction: what the total should be after normalization
+     
+     - Returns: the total probability before normalizing
+     
+     - Throws: `Errors.TotalProbabilityZero` is thrown if, before normalization, the sum of all the probabilities is zero
+     */
+    func normalize(fraction: Double = 1.0) throws -> Double {
+        let sum = total()
+        
+        if sum == 0 {
+            throw Errors.TotalProbabilityZero
+        }
+        
+        let factor = fraction / sum
+        
+        for (key,value) in dict {
+            dict[key] = value * factor
+        }
+        
+        return sum
+    }
 }
--- SwiftThinkBayesTests/SwiftThinkBayesTests.swift
+++ SwiftThinkBayesTests/SwiftThinkBayesTests.swift
@@ -10,9 +10,9 @@ import XCTest
 @testable import SwiftThinkBayes
 
 class SwiftThinkBayesTests: XCTestCase {
-    let epsilson = 10e-7
+    let epsilon = 10e-7
     
-    func testSection2_2() {
+    func testSection2_2() throws {
         let pmf = Pmf<String>()
         
         pmf.set(key: "Bowl 1", value: 0.5)
@@ -20,21 +20,36 @@ class SwiftThinkBayesTests: XCTestCase {
             XCTFail()
             return
         }
-        XCTAssert(abs(result1 - 0.5) < epsilson )
+        XCTAssert(abs(result1 - 0.5) < epsilon )
         
         pmf.set(key: "Bowl 2", value: 0.5)
         guard let result2 = pmf.dict["Bowl 2"] else {
             XCTFail()
             return
         }
-        XCTAssert(abs(result2 - 0.5) < epsilson )
+        XCTAssert(abs(result2 - 0.5) < epsilon )
         
         pmf.mult(key: "Bowl 1", factor: 0.75)
         guard let result3 = pmf.dict["Bowl 1"] else {
             XCTFail()
             return
         }
-        XCTAssert(abs(result3 - 0.5*0.75) < epsilson )
+        XCTAssert(abs(result3 - 0.5*0.75) < epsilon )
+
+        pmf.mult(key: "Bowl 2", factor: 0.5)
+        guard let result4 = pmf.dict["Bowl 2"] else {
+            XCTFail()
+            return
+        }
+        XCTAssert(abs(result4 - 0.5*0.5) < epsilon )
+        
+        let total = pmf.total()
+        XCTAssert(abs(total - (0.5*0.75 + 0.5*0.5)) < epsilon)
+
+        // Normalise
+        let before = try pmf.normalize()
+        XCTAssert(abs(before - (0.5*0.75 + 0.5*0.5)) < epsilon)
+        XCTAssert(abs(pmf.total() - 1) < epsilon, "Total after normalization should be 1.0")
 
     }
 }
